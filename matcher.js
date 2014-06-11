329f63e4877b0cbc21069968d0773173a6deed63
Ted Meyer <tmathmeyer@gmail.com>
2014-06-11 15:23:11 -0400

    cleaned up the JS so that I can add more features, looking into local storage

--- matcher.js
+++ matcher.js
@@ -1,7 +1,7 @@
 builtins = [
 	{
 		tags: ["/g/", "technology", "install gentoo"],
-		title: "technolo/g",
+		title: "technology",
 		url: "http://4chan.org/g/"
 	},
 	{
@@ -33,6 +33,11 @@ builtins = [
 		tags: ["pomfse", "ohayou", "hosting", "files"],
 		title: "pomf.se",
 		url: "http://pomf.se/"
+	},
+	{
+		tags: ["comics", "xkcd"],
+		title: "xkcd",
+		url: "http://xkcd.com"
 	}
 ];
 
@@ -69,7 +74,10 @@ var engines = [
 
 
 
-matchToString = function(query, search) {
+
+// match a string to a query by more than just nieve indexOf,
+// the way that sublime does it
+queryInString = function(query, search) {
 	var ndx = 0;
 	for (var i = 0, len = query.length; i < len; i++) {
 		ndx = search.indexOf(query[i]);
@@ -81,19 +89,10 @@ matchToString = function(query, search) {
 	return ndx != -1;
 }
 
-getMatchingResults = function(query) {
-	return builtins.filter(function(each){
-		var set = false;
-		for (var i = 0, len = each.tags.length; i < len; i++) {
-			if (matchToString(query, each.tags[i])){
-				set = true;
-				i = len;
-			}
-		}
-		return set;
-	});
-}
 
+
+// block the use of the arrow keys for anything except
+// what is used in the keyup listener
 document.addEventListener("keydown", function(e){
 	if (e.keyCode > 36 && e.keyCode < 41) {
 		e.preventDefault();
@@ -101,86 +100,126 @@ document.addEventListener("keydown", function(e){
 	}
 });
 
+
+
+// listen for keys in the text box
 document.addEventListener("keyup", function(e){
-	console.log(e.keyCode);
-	if (e.keyCode == 13) {
-		var sel = document.getElementById("selected");
-		if (sel) {			
-			document.location.href = sel.getElementsByClassName("link")[0].href
-		} else {
-			document.location.href = "https://www.google.com/#q="+encodeURIComponent(document.getElementById("mainsearch").value);
-		}
-	} else if (e.keyCode == 38) {
-		upArrow();
-		e.stopPropagation();
-	} else if (e.keyCode == 40) {
-		downArrow();
-		e.stopPropagation();
-	} else {
-		var embed = document.getElementById("searchresults");
-		var searchterm = document.getElementById("mainsearch").value;
-		embed.innerHTML = process(searchterm);
-		currentIndex = 0;
-		upArrow();
+	switch(e.keyCode) {
+		case 91: // super
+		case 16: // shift
+			break;
+		case 13:
+			var sel = document.getElementById("selected");
+			if (sel) {
+				document.location.href = sel.getElementsByClassName("link")[0].href
+			} else {
+				document.location.href = "https://www.google.com/#q="+encodeURIComponent(document.getElementById("mainsearch").value);
+			} break;
+		case 38:
+			arrows.UP();
+			break;
+		case 40:
+			arrows.DOWN();
+			break;
+		default:
+			var embed = document.getElementById("searchresults");
+			var searchterm = document.getElementById("mainsearch").value;
+			embed.innerHTML = process(searchterm);
+			currentIndex = 0;
+			arrows.UP();
 	}
 });
 
-upArrow = function() {
-	var results = document.getElementsByClassName("result");
-	if (results[0]) {
-		results[currentIndex].removeAttribute("id");
-		currentIndex = currentIndex==0?0:currentIndex-1;
-		results[currentIndex].setAttribute("id", "selected");
-	}
-}
 
-downArrow = function() {
-	var results = document.getElementsByClassName("result");
-	if (results[0]) {
-		results[currentIndex].removeAttribute("id");
-		currentIndex = currentIndex+1;
-		if (currentIndex == results.length) {
-			currentIndex = currentIndex-1;
+
+
+// arrow listeners for the up and down arrows
+var arrows = {
+	UP: function() {
+		var results = document.getElementsByClassName("result");
+		if (results[0]) {
+			results[currentIndex].removeAttribute("id");
+			currentIndex = currentIndex==0?0:currentIndex-1;
+			results[currentIndex].setAttribute("id", "selected");
 		}
-		results[currentIndex].setAttribute("id", "selected");
+	},
+	DOWN: function() {
+		var results = document.getElementsByClassName("result");
+		if (results[0]) {
+			results[currentIndex].removeAttribute("id");
+			currentIndex = currentIndex+1;
+			if (currentIndex == results.length) {
+				currentIndex = currentIndex-1;
+			}
+			results[currentIndex].setAttribute("id", "selected");
+		}
 	}
 }
 
 
-
-processMetaOption = function(str) {
+processSearch = function(str) {
 	var parts = str.split(":");
 
-	return engines.filter(function(each){
-		return matchToString(parts[0], each.title);
-	}).map(function(each){
+	return htmlreducer(function(each){
+		return queryInString(parts[0], each.title);
+	}, function(each) {
 		var encoded = "NaN";
 		if (parts[1]) {
 			encoded = parts[1].trim();
 		}
-		return "<div class='result'><a class='link' href='"+each.url+encodeURIComponent(encoded)+"'>" + each.title + "</a></div>";
-	}).reduce(function(previousValue, currentValue) {
-		return previousValue + currentValue;
-	}, "");
+		return {
+			url: each.url + encodeURIComponent(encoded),
+			title: each.title
+		};
+	}, engines);
 }
 
-process = function(string) {
-	if (string) {
-		if (string[0] == ':') {
-			return processMetaOption(string.substring(1));
+processNormal = function(query) {
+	return htmlreducer(function(each){
+		var set = false;
+		for (var i = 0, len = each.tags.length; i < len; i++) {
+			if (queryInString(query, each.tags[i])){
+				set = true;
+				i = len;
+			}
 		}
-		return getMatchingResults(string).map(function(each){
-			return "<div class='result'><a class='link' href='"+each.url + "'>" + each.title + "</a></div>";
-		}).reduce(function(previousValue, currentValue){
-			return previousValue + currentValue;
-		}, "");
-	} else {
+		return set;
+	}, function(each) {
+		return each;
+	}, builtins);
+}
+
+htmlreducer = function(filterer, mapper, source) {
+	return source.filter(filterer).map(function(each){
+		results = mapper(each);
+		return "<div class='result'><a class='link' href='"+results.url + "'>" + results.title + "</a></div>";
+	}).reduce(function(a, b){
+		return a+b;
+	});
+}
+
+
+
+
+process = function(string) {
+	if (!string) {
 		return "";
+	}
+
+	switch (string[0]) {
+		case "@":
+			return processSearch(string.substring(1));
+		default:
+			return processNormal(string);
 	}
 }
 
+
+// focus the search bar
+// add the type suggestion into the place where results go
 window.onload = function() {
 	document.getElementById("mainsearch").focus();
+	document.getElementById("searchresults").innerHTML = "<div id = 'typesomething'> Type Something </div>"
 };
 
 
