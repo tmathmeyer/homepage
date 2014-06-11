87de7b4655225c0956599bb8006aaac7ded06bd6
Ted Meyer <tmathmeyer@gmail.com>
2014-06-11 01:10:16 -0400

    added search engines, queried with ::'s. non-matching search also goes to google now

--- matcher.js
+++ matcher.js
@@ -1,43 +1,76 @@
 builtins = [
 	{
-		names: ["/g/", "technology"],
+		tags: ["/g/", "technology"],
 		title: "technology",
 		url: "http://4chan.org/g/"
 	},
 	{
-		names: ["google"],
-		title: "google",
-		url: "http://google.com"
-	},
-	{
-		names: ["facebook", "fb"],
+		tags: ["facebook", "fb"],
 		title: "facebook",
 		url: "http://facebook.com"
 	},
 	{
-		names: ["github.com", "github", "git", "code"],
+		tags: ["github.com", "github", "git", "code"],
 		title: "github",
 		url: "http://github.com"
 	},
 	{
-		names: ["gmail.com", "gmail", "mail"],
+		tags: ["gmail.com", "gmail", "mail"],
 		title: "gmail",
 		url: "http://gmail.com"
 	},
 	{
-		names: ["netflix", "movies"],
+		tags: ["netflix", "movies"],
 		title: "netflix",
 		url: "http://netflix.com"
 	},
 	{
-		names: ["etho", "ethoslab"],
+		tags: ["etho", "ethoslab"],
 		title: "etho's lab",
 		url: "https://www.youtube.com/user/EthosLab/videos"
+	},
+	{
+		tags: ["pomfse", "ohayou", "hosting", "files"],
+		title: "pomf.se",
+		url: "http://pomf.se/"
 	}
 ];
 
 var topResult;
 var currentIndex = 0;
+
+var engines = [
+	{
+		title: "google",
+		url: 'https://www.google.com/#q='
+	},
+	{
+		title: "duck duck go",
+		url: 'https://next.duckduckgo.com/?q='
+	},
+	{
+		title: "wolfram|alpha",
+		url: 'https://www.wolframalpha.com/input/?i='
+	},
+	{
+		title: "wikipedia",
+		url: 'https://wikipedia.org/wiki/Special:Search?search='
+	},
+	{
+		title: "maps",
+		url: 'https://www.google.com/maps/place/'
+	},
+	{
+		title: "youtube",
+		url: 'https://youtube.com/results?search_query='
+	},
+	{
+		title: "images",
+		url: 'https://www.google.com/search?tbm=isch&q='
+	}
+];
+
+
 
 
 matchToString = function(query, search) {
@@ -55,8 +88,8 @@ matchToString = function(query, search) {
 getMatchingResults = function(query) {
 	return builtins.filter(function(each){
 		var set = false;
-		for (var i = 0, len = each.names.length; i < len; i++) {
-			if (matchToString(query, each.names[i])){
+		for (var i = 0, len = each.tags.length; i < len; i++) {
+			if (matchToString(query, each.tags[i])){
 				set = true;
 				i = len;
 			}
@@ -65,22 +98,28 @@ getMatchingResults = function(query) {
 	});
 }
 
+document.addEventListener("keydown", function(e){
+	if (e.keyCode > 36 && e.keyCode < 41) {
+		e.preventDefault();
+		e.stopPropagation();
+	}
+});
 
 document.addEventListener("keyup", function(e){
 	console.log(e.keyCode);
 	if (e.keyCode == 13) {
-		if (topResult) {
-			var sel = document.getElementById("selected");
-			if (sel) {			
-				document.location.href = sel.getElementsByClassName("link")[0].href
-			} else {
-				console.log("well shit");
-			}
+		var sel = document.getElementById("selected");
+		if (sel) {			
+			document.location.href = sel.getElementsByClassName("link")[0].href
+		} else {
+			document.location.href = "https://www.google.com/#q="+encodeURIComponent(document.getElementById("mainsearch").value);
 		}
 	} else if (e.keyCode == 38) {
 		upArrow();
-	} else if (e.keyCode == 40) { //down
+		e.stopPropagation();
+	} else if (e.keyCode == 40) {
 		downArrow();
+		e.stopPropagation();
 	} else {
 		var embed = document.getElementById("searchresults");
 		var searchterm = document.getElementById("mainsearch").value;
@@ -92,26 +131,37 @@ document.addEventListener("keyup", function(e){
 
 upArrow = function() {
 	var results = document.getElementsByClassName("result");
-	results[currentIndex].removeAttribute("id");
-	currentIndex = currentIndex==0?0:currentIndex-1;
-	results[currentIndex].setAttribute("id", "selected");
+	if (results[0]) {
+		results[currentIndex].removeAttribute("id");
+		currentIndex = currentIndex==0?0:currentIndex-1;
+		results[currentIndex].setAttribute("id", "selected");
+	}
 }
 
 downArrow = function() {
 	var results = document.getElementsByClassName("result");
-	results[currentIndex].removeAttribute("id");
-	currentIndex = currentIndex+1;
-	if (currentIndex == results.length) {
-		currentIndex = currentIndex-1;
+	if (results[0]) {
+		results[currentIndex].removeAttribute("id");
+		currentIndex = currentIndex+1;
+		if (currentIndex == results.length) {
+			currentIndex = currentIndex-1;
+		}
+		results[currentIndex].setAttribute("id", "selected");
 	}
-	results[currentIndex].setAttribute("id", "selected");
 }
 
 
 
-processMetaOption = function(string) {
-	console.log(string);
-	return "";
+processMetaOption = function(str) {
+	var parts = str.split(":");
+
+	return engines.filter(function(each){
+		return matchToString(parts[0], each.title);
+	}).map(function(each){
+		return "<div class='result'><a class='link' href='"+each.url+encodeURIComponent(parts[1].trim())+"'>" + each.title + "</a></div>";
+	}).reduce(function(previousValue, currentValue) {
+		return previousValue + currentValue;
+	}, "");
 }
 
 process = function(string) {
@@ -119,9 +169,7 @@ process = function(string) {
 		if (string[0] == ':') {
 			return processMetaOption(string.substring(1));
 		}
-		results = getMatchingResults(string);
-		topResult = results[0];
-		return results.map(function(each){
+		return getMatchingResults(string).map(function(each){
 			return "<div class='result'><a class='link' href='"+each.url + "'>" + each.title + "</a></div>";
 		}).reduce(function(previousValue, currentValue){
 			return previousValue + currentValue;
