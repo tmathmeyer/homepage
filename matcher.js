e7f521a9d1d899d424d8c78adaa76b9b2cd75a3c
Ted Meyer <tmathmeyer@gmail.com>
2014-06-10 22:50:03 -0400

    added background, better filtering, 'mascots', selectors with : (still to be fully implemented

--- matcher.js
+++ matcher.js
@@ -18,10 +18,26 @@ builtins = [
 		names: ["github.com", "github", "git", "code"],
 		title: "github",
 		url: "http://github.com"
+	},
+	{
+		names: ["gmail.com", "gmail", "mail"],
+		title: "gmail",
+		url: "http://gmail.com"
+	},
+	{
+		names: ["netflix", "movies"],
+		title: "netflix",
+		url: "http://netflix.com"
+	},
+	{
+		names: ["etho", "ethoslab"],
+		title: "etho's lab",
+		url: "https://www.youtube.com/user/EthosLab/videos"
 	}
 ];
 
 var topResult;
+var currentIndex = 0;
 
 
 matchToString = function(query, search) {
@@ -51,29 +67,72 @@ getMatchingResults = function(query) {
 
 
 document.addEventListener("keyup", function(e){
+	console.log(e.keyCode);
 	if (e.keyCode == 13) {
 		if (topResult) {
-			document.location.href = topResult.url;
+			var sel = document.getElementById("selected");
+			if (sel) {			
+				document.location.href = sel.getElementsByClassName("link")[0].href
+			} else {
+				console.log("well shit");
+			}
 		}
+	} else if (e.keyCode == 38) {
+		upArrow();
+	} else if (e.keyCode == 40) { //down
+		downArrow();
 	} else {
 		var embed = document.getElementById("searchresults");
 		var searchterm = document.getElementById("mainsearch").value;
 		embed.innerHTML = process(searchterm);
-
+		currentIndex = 0;
+		upArrow();
 	}
 });
 
+upArrow = function() {
+	var results = document.getElementsByClassName("result");
+	results[currentIndex].removeAttribute("id");
+	currentIndex = currentIndex==0?0:currentIndex-1;
+	results[currentIndex].setAttribute("id", "selected");
+}
+
+downArrow = function() {
+	var results = document.getElementsByClassName("result");
+	results[currentIndex].removeAttribute("id");
+	currentIndex = currentIndex+1;
+	if (currentIndex == results.length) {
+		currentIndex = currentIndex-1;
+	}
+	results[currentIndex].setAttribute("id", "selected");
+}
+
+
+
+processMetaOption = function(string) {
+	console.log(string);
+	return "";
+}
+
 process = function(string) {
 	if (string) {
+		if (string[0] == ':') {
+			return processMetaOption(string.substring(1));
+		}
 		results = getMatchingResults(string);
 		topResult = results[0];
 		return results.map(function(each){
-			return "<div class='result'><a href='"+each.url + "'>" + each.title + "</a></div>";
+			return "<div class='result'><a class='link' href='"+each.url + "'>" + each.title + "</a></div>";
 		}).reduce(function(previousValue, currentValue){
 			return previousValue + currentValue;
 		}, "");
 	} else {
 		return "";
 	}
-
 }
+
+window.onload = function() {
+	document.getElementById("mainsearch").focus();
+};
+
+
